c7bd4948d937032267700c54f2afb8052c6fefa9
Nemat Bhullar <n.bhullar@student.unsw.edu.au>
2023-05-25 12:46:19 +1000

    Fixed up button hover

---
+++ src/components/Web.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Web = () => {
+  <div>
+    <svg width="96" height="73" viewBox="0 0 96 73" fill="current" stroke="current" xmlns="http://www.w3.org/2000/svg">
+      <rect x="2" y="2" width="92" height="69" rx="6" stroke="#36B5C6" stroke-width="4"/>
+      <path d="M1.5 18.5H93.5" stroke="#36B5C6" stroke-width="4"/>
+      <circle cx="11.5" cy="10.5" r="2.5" fill="#36B5C6"/>
+      <circle cx="20.5" cy="10.5" r="2.5" fill="#36B5C6"/>
+      <circle cx="29.5" cy="10.5" r="2.5" fill="#36B5C6"/>
+      <path d="M33 35L20 46.2444L33 57" stroke="#36B5C6" stroke-width="4" stroke-linecap="round"/>
+      <path d="M60 35L73 46.2444L60 57" stroke="#36B5C6" stroke-width="4" stroke-linecap="round"/>
+      <path d="M53.5 34.5L41 58" stroke="#36B5C6" stroke-width="4" stroke-linecap="round"/>
+    </svg>
+  </div>
+  
+}
+
+export default Web;
