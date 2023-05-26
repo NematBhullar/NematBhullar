d09e7d891317cafcdef032003eb92de040c32a06
Nemat Bhullar <n.bhullar@student.unsw.edu.au>
2023-05-26 22:31:52 +1000

    Added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../assets/logo-transparent.png';
+import logo from '../assets/logo.png';
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
