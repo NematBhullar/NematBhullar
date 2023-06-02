21694ecf43e463c60bc84e524ec40203297bfd88
Nemat Bhullar <n.bhullar@student.unsw.edu.au>
2023-06-02 16:30:41 +1000

    Added resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from '../assets/logo.png';
+import resume from '../assets/resume.pdf';
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
@@ -13,7 +14,7 @@ const Navbar = () => {
         <li className="each-link"><a href="/#projects" className="navbar-links">Projects</a></li>
         <li className="each-link"><a href="/#designs" className="navbar-links">Designs</a></li>
         <li className="each-link"><a href="/#contact" className="navbar-links">Contact</a></li>
-        <li className="resume-link">Resume</li>
+        <li className="resume-link"><a href={resume}>Resume</a></li>
       </ul>
       <ul className="all-links-small">
         <li className="each-link"><a href="/#home" className="navbar-links">H</a></li>
